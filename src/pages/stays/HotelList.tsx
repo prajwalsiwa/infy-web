00f086f3e4852f8917f853f480c1e89309332bd1
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-06-01 07:54:42 +0545

    feat: add card animation

--- src/pages/stays/HotelList.tsx
+++ src/pages/stays/HotelList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import StaysCard from "@/components/SearchDetails/stays/Card/Card";
 import { Button } from "@/components/ui/button";
 import SearchRecommendations from "@/components/SearchDetails/ui/ListSection/SearchRecommendations";
@@ -9,8 +10,11 @@ import {
   useGetHotelRecommendationsQuery,
   useGetTrendsQuery,
 } from "@/redux/services/homeApi";
+import CardSkeleton from "@/components/SearchDetails/stays/Card/CardSkeleton";
+
 import { useHotelListQuery } from "@/redux/services/staysApi";
 import { useSearchParams } from "react-router-dom";
+
 const options = [
   {
     label: "Recommended",
@@ -21,45 +25,41 @@ const options = [
 function Stays() {
   const [searchParams] = useSearchParams();
   const queryParams = Object.fromEntries(searchParams.entries());
-  const { data: hotelList } = useHotelListQuery(queryParams);
-  const { data: trendList, isLoading, error } = useGetTrendsQuery();
+
+  const {
+    data: hotelList,
+    isLoading: isHotelsLoading,
+    error: hotelError,
+  } = useHotelListQuery(queryParams);
+
+  const {
+    data: trendList,
+    isLoading: isTrendsLoading,
+    error: trendsError,
+  } = useGetTrendsQuery();
+
   const {
     data: recommendations,
     isLoading: isRecommendationsLoading,
-    error: isRecommendataionError,
+    error: recommendationsError,
   } = useGetHotelRecommendationsQuery();
 
-  const recommendationList = recommendations?.results;
-
-  if (isLoading) <div>Loading...</div>;
-  if (error) <div>Loading...</div>;
-
-  if (isRecommendationsLoading) <div>Loading...</div>;
-  if (isRecommendataionError) <div>Loading...</div>;
-
-  const handleSortChange = () => {};
-
-  const hotelListResults = hotelList?.results ? hotelList?.results : [];
+  const recommendationList = recommendations?.results ?? [];
+  const hotelListResults = hotelList?.results ?? [];
 
   const staysCardList = hotelListResults.map((hotel) => ({
     id: hotel.id ?? 0,
     hotelImage: hotel?.photo_url,
-    // hotelSubImageList: hotel.sub_image_list?.map((subImage) => ({
-    //   id: subImage.id,
-    //   subImage: subImage.sub_image_url,
-    // })),
     title: hotel.name,
     location: `${hotel.location.city}, ${hotel.location.country}`,
     serviceList:
-      (hotel?.amenities &&
-        hotel?.amenities.map((service) => ({
-          id: service.id,
-          title: service.title,
-          category: service.category,
-          amenities_for: service.amenities_for,
-          icon: service.icon,
-        }))) ??
-      [],
+      hotel?.amenities?.map((service) => ({
+        id: service.id,
+        title: service.title,
+        category: service.category,
+        amenities_for: service.amenities_for,
+        icon: service.icon,
+      })) ?? [],
     type: hotel.category.name,
     rating: hotel.ratings ?? 0,
     reviews: hotel.user_review_count ?? 0,
@@ -70,13 +70,18 @@ function Stays() {
       : 0,
   }));
 
+  const handleSortChange = () => {};
+
   return (
     <div className="w-full">
-      <div className="left-side hotels pr-2  grid lg:grid-cols-[25%_75%] grid-cols-1  h-full   ">
-        <div className="left lg:flex flex-col gap-6  w-full hidden pr-3    ">
+      <div className="left-side hotels pr-2 grid lg:grid-cols-[25%_75%] grid-cols-1 h-full">
+        {/* Left Filters and Map */}
+        <div className="left lg:flex flex-col gap-6 w-full hidden pr-3">
           <MapSection />
           <StaysFilter />
         </div>
+
+        {/* Right Main Content */}
         <div className="xl:pl-12 md:pl-4 w-full">
           <ListHeader
             itemCount={recommendationList?.length}
@@ -85,14 +90,22 @@ function Stays() {
             onSortChange={handleSortChange}
           />
           <div className="flex flex-col gap-6 w-full">
-            <div className=" relative flex flex-col gap-4 w-full ">
-              <div>
-                <div className="flex  flex-col gap-2 w-full ">
-                  {staysCardList?.map((hotel) => (
+            {/* Stays Card Section */}
+            <div className="relative flex flex-col gap-4 w-full">
+              <div className="flex flex-col gap-2 w-full">
+                {isHotelsLoading ? (
+                  <>
+                    <CardSkeleton />
+                    <CardSkeleton />
+                    <CardSkeleton />
+                    <CardSkeleton />
+                  </>
+                ) : staysCardList.length > 0 ? (
+                  staysCardList.map((hotel) => (
                     <StaysCard
                       key={hotel.id}
                       id={hotel.id}
-                      hotelImage={hotel?.hotelImage}
+                      hotelImage={hotel.hotelImage}
                       title={hotel.title}
                       location={hotel.location}
                       serviceList={hotel.serviceList}
@@ -103,9 +116,14 @@ function Stays() {
                       discountedPrice={hotel.discountedPrice}
                       discountPercent={hotel.discountPercent}
                     />
-                  ))}
-                </div>
+                  ))
+                ) : (
+                  <div className="text-gray-500 text-center py-6">
+                    No stays found.
+                  </div>
+                )}
               </div>
+
               <div>
                 <Button
                   className="w-full bg-white border rounded-sm border-primary-dark text-primary-dark"
@@ -116,6 +134,7 @@ function Stays() {
               </div>
             </div>
 
+            {/* Recommendations and Destinations */}
             <div className="flex flex-col gap-6">
               <div>
                 <SearchRecommendations
