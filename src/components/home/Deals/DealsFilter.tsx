c8584969533b1a6c3f851de95b39e6fe4172d97f
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-06-02 17:51:27 +0545

    feat: integrate filter

--- src/components/home/Deals/DealsFilter.tsx
+++ src/components/home/Deals/DealsFilter.tsx
@@ -1,10 +1,58 @@
 import { CheckInOutPicker } from "@/components/ui/check-in-out-picker";
 import { GuestPicker } from "@/components/ui/guest-picker";
 import { Button } from "@/components/ui/button";
+import { addDays } from "date-fns";
+import { useCallback, useState, SetStateAction } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function DealsFilter() {
+  const [date, setDate] = useState({
+    from: addDays(new Date(), 1),
+    to: addDays(new Date(), 2),
+  });
+
+  const [guestValues, setGuestValues] = useState({
+    adults: 0,
+    children: 0,
+    infants: 0,
+  });
+
+  const formatDate = useCallback(
+    (date: Date) => date.toISOString().split("T")[0],
+    []
+  );
+
+  const handleGuestChange = useCallback(
+    (
+      updatedValues: SetStateAction<{
+        adults: number;
+        children: number;
+        infants: number;
+      }>
+    ) => {
+      setGuestValues(updatedValues);
+    },
+    []
+  );
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const handleSearch = () => {
-    console.warn("Searching deals...");
+    const checkin_date = formatDate(date.from);
+    const checkout_date = formatDate(date.to);
+
+    const params = new URLSearchParams(location.search);
+    params.set("checkin_date", checkin_date);
+    params.set("checkout_date", checkout_date);
+    params.set("adults", guestValues.adults.toString());
+    params.set("children", guestValues.children.toString());
+    params.set("infants", guestValues.infants.toString());
+
+    navigate({
+      pathname: location.pathname,
+      search: params.toString(),
+    });
   };
 
   return (
@@ -13,7 +61,7 @@ function DealsFilter() {
       <div className="flex flex-col  sm:flex-row items-stretch sm:items-center gap-4 flex-grow">
         {/* Date Picker */}
         <div className="!w-full sm:w-[15rem]">
-          <CheckInOutPicker date={undefined} setDate={undefined} />
+          <CheckInOutPicker date={date} setDate={setDate} />
         </div>
 
         {/* Vertical Separator on large screens */}
@@ -21,13 +69,7 @@ function DealsFilter() {
 
         {/* Guest Picker */}
         <div className="w-[15rem] ">
-          <GuestPicker
-            values={{
-              adults: 0,
-              children: 0,
-              infants: 0,
-            }}
-          />
+          <GuestPicker values={guestValues} onChange={handleGuestChange} />
         </div>
       </div>
 
