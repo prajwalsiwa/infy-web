cbdf029a88c04038833cbb7100b27fec2ade0c78
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-06-04 08:36:48 +0545

    feat: integrate memebership plan

--- src/components/ListYourProperty/CompleteListing/MembershipCard.tsx
+++ src/components/ListYourProperty/CompleteListing/MembershipCard.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getMembershipPlansResponse } from "@/redux/features/Types/listYourProperty";
+import { useFormContext } from "react-hook-form";
 
 interface MembershipPlan {
   plans: getMembershipPlansResponse[] | undefined;
 }
 
 const MembershipCard: React.FC<MembershipPlan> = ({ plans }) => {
+  const { setValue } = useFormContext();
   const [selectedPlan, setSelectedPlan] = useState<string>("Basic Membership");
+
+  const selectedPlanObj = plans?.find((plan) => plan.name === selectedPlan);
+
+  useEffect(() => {
+    if (selectedPlanObj?.id) {
+      setValue("membership.membership", selectedPlanObj.id);
+    }
+  }, [selectedPlanObj, setValue]);
 
   return (
     <div className="flex w-full justify-center gap-4">
@@ -14,14 +24,20 @@ const MembershipCard: React.FC<MembershipPlan> = ({ plans }) => {
         <div
           key={plan.name}
           onClick={() => setSelectedPlan(plan.name)}
-          className={`cursor-pointer w-full border rounded-lg p-4 transition-shadow ${
+          className={`cursor-pointer w-full border relative rounded-lg p-4 transition-shadow ${
             selectedPlan === plan.name
               ? "border-primary border-2 shadow-lg"
               : "border-gray-300"
           }`}
         >
+          {plan.id === 3 && (
+            <div className="text-sm  h-8 w-full absolute text-primary font-semibold bg-primary-light  right-0 top-0 rounded-tl-lg rounded-tr-lg flex items-center justify-center ">
+              Popular
+            </div>
+          )}
+
           {/* Selection Icon */}
-          <div className="flex justify-end mt-2">
+          <div className="flex justify-end mt-8">
             <i
               className={`material-icons font-bold text-md ${
                 selectedPlan === plan.name ? "text-primary" : "text-grey-300"
@@ -34,21 +50,21 @@ const MembershipCard: React.FC<MembershipPlan> = ({ plans }) => {
           {/* Plan Details */}
           <div className="flex flex-col-reverse">
             <h3 className="text-lg text-gray-dark">{plan.name}</h3>
-            {/* {plan.popular && (
-              <span className="text-sm text-primary font-semibold">
-                (Popular)
-              </span>
-            )} */}
           </div>
 
           {/* Price Section */}
           <p className="text-lg text-gray-dark font-medium mt-2">
             {plan.price}
-            <span className="text-gray">/{plan.expires_on / 30 && "month"}</span>
+            <span className="text-gray">
+              /{plan.expires_on && plan.expires_on / 30 >= 1 ? "month" : "day"}
+            </span>
           </p>
 
           {/* Plan Description */}
-          <p className="text-gray-dark mt-2">{plan.description}</p>
+          <div
+            className="text-gray-dark mt-2 list-disc pl-5 [&>ul]:list-disc [&>ul]:pl-4 [&>li]:mb-1"
+            dangerouslySetInnerHTML={{ __html: plan.description }}
+          />
         </div>
       ))}
     </div>
